5cf080fdebc89498af3fc11b5c39bdd3d0b916db
marceloguilherme100 <marcelo.guilherme100@gmail.com>
2024-05-11 18:28:22 -0300

    realizando condicionais

---
+++ contre-fluxo/PlanoOperadora.java
@@ -0,0 +1,19 @@
+public class PlanoOperadora {
+    public static void main(String[] args) {
+    String plano = "M";
+
+    switch (plano) {
+        case "T" : {
+        System.out.println("5 GB Youtube");
+        }
+        case "M" : {
+            System.out.println("Whatsapp e Instagram gratis");
+            }
+        case "B" : {
+                System.out.println("100 minutos de ligacao");
+                }    
+        
+         }
+
+    }
+}
